5828e2384b2d0c4cd18accddcec5024b109d951e
APG-PPTG <53134599+APG-PPTG@users.noreply.github.com>
2021-01-13 19:11:59 -0700

    Update testingChallenge.java

--- testingChallenge.java
+++ testingChallenge.java
@@ -19,13 +19,11 @@ public class testingChallenge {
 		driver.get("http://automationpractice.com");
 		driver.manage().window().maximize();
 		Actions action = new Actions(driver);
-		//WebElement we1=driver.findElement(By.xpath("//header/div[3]/div[1]/div[1]/div[6]/ul[1]/li[1]/a[1]"));
 		WebElement we1=driver.findElement(By.cssSelector("a[title*='Women']"));
 		action.moveToElement(we1).build().perform();
 		driver.findElement(By.cssSelector("a[title*='Summer Dresses']")).click();
 		Thread.sleep(5000);
 		List<WebElement> option= driver.findElements(By.cssSelector("a[itemprop*='url']"));
-		//System.out.println(option);
 		for (WebElement we : option) {
 			action.moveToElement(we).build().perform();
 			driver.findElement(By.cssSelector("a[title='Add to cart']")).click();
@@ -33,13 +31,7 @@ public class testingChallenge {
 		}
 		Thread.sleep(5000);
 		driver.findElement(By.cssSelector("a.button-medium")).click();
-	//	Thread.sleep(5000);
-	//	driver.findElement(By.cssSelector("a.standard-checkout")).click();
-		//driver.findElement(By.linkText("a[href*='=order']")).click();
 		Thread.sleep(5000);
-		//driver.findElement(By.xpath(("[class='button btn btn-default']"))).click();
-		System.out.println("Print1");
-		//driver.findElement(By.cssSelector("a.Proceed to checkout")).click();
 		driver.findElement(By.cssSelector("[class='button btn btn-default standard-checkout button-medium']")).click();
 		System.out.println("Print2");
 		driver.findElement(By.id("email_create")).sendKeys("peter10@gmail.com");
@@ -72,13 +64,11 @@ public class testingChallenge {
 		driver.findElement(By.id("alias")).sendKeys("Peter");
 		driver.findElement(By.id("submitAccount")).click();
 		driver.findElement(By.name("processAddress")).click();
-		//driver.findElement(By.cssSelector("[class='button btn btn-default standard-checkout button-medium']")).click();
 		driver.findElement(By.id("cgv")).click();
 		driver.findElement(By.cssSelector("[class='button btn btn-default standard-checkout button-medium']")).click();
 		driver.findElement(By.className("bankwire")).click();
 		driver.findElement(By.xpath("//*[@id=\"cart_navigation\"]/button")).click();
 		String OrderConfirm=driver.findElement(By.id("order-confirmation")).getText();
-		//System.out.println(OrderConfirm);
 		if(OrderConfirm.contains("Your order on My Store is complete"))
 		{
 			System.out.println("Order Completed");
